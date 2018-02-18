48786f1417fce6854836bef5f648fda94de2c4d1
Michelle Becerra <michel8@vt.edu>
2018-02-17 17:45:28 -0800

    first

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+
+//heroku
+app.set('port', (process.env.PORT || 5000))
+//handles jason how it needs to
+app.use(bodyParser.bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.get('/', function(req, res){
+	res.send("Hello ");
+});
+
+app.get('/webhook/', function(req, rest){
+	if(req.query['hub.verify_token'] ===
+		'my_token'){
+			res.send(req.query['hub.challenge'])
+		}
+	res.send('No entry')
+});
+
+app.listen(app.get('port', function(){
+	console.log('running on port', app.get('port'))
+});
